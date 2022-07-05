86a0a066c03a7e9b18ec4296b09f6641b1c1de9f
Lenard Villestas <lenard.villestas@outlook.com>
2022-07-05 15:57:03 -0600

    added Post js for http example (mod 9)

---
+++ src/http/PostList.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+
+class PostList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+                this.setState({ posts: response.data })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    render() {
+        const { posts } = this.state;
+
+
+        return (
+            <div> <h1>List of posts</h1>
+
+                {posts.length ?
+                    posts.map(post => <div key={post.id}> {post.title} </div>):null}
+            </div>
+
+
+
+        )
+    }
+
+
+}
+
+
+export default PostList
