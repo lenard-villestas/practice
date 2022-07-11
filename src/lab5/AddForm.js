9270c168ed3e6face195ef593191346c4ae0e543
Lenard Villestas <lenard.villestas@outlook.com>
2022-07-11 01:05:37 -0600

    added form and table elements to addfor.js

--- src/lab5/AddForm.js
+++ src/lab5/AddForm.js
@@ -12,12 +12,30 @@ class AddForm extends Component {
     submitHandler = (e) => {
 
     }
+    changeHandler = (e) => {
+        this.setState({[e.target.id]:e.target.value})
+    }
 
   render() {
+    let{id,title} = this.state;
     return (
       <div>
         <form onSubmit={this.submitHandler}>
-
+        <table>
+            <tr>
+                <td><label htmlFor='id'>ID: </label></td>
+                <td><input id='id' type='number' onChange={this.changeHandler} value={id}></input></td>
+            </tr>
+            <tr>
+                <td><label htmlFor='title'>Title: </label></td>
+                <td><input id='title' type='text' onChange={this.changeHandler} value={title}></input></td>
+            </tr>
+            <tr>
+               <td colSpan='2'>
+               <button type='submit'>Submit</button>
+                </td> 
+            </tr>
+        </table>
         </form>
       </div>
     )
